e947e3cc659241a3c67a053fa5ad63ba3234a229
Arunabh Verma <arunabhverma01@gmail.com>
2022-10-12 22:39:15 +0530

    Related commit(s): new task

--- App.js
+++ App.js
@@ -1,166 +1,90 @@
-import React, {useState} from 'react';
-import {
-  Text,
-  View,
-  Modal,
-  StyleSheet,
-  TouchableOpacity,
-  FlatList,
-} from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 
-const isColor = (h) => {
-  var a = parseInt(h,16);
-  return (a.toString(16) === h)
+const initialState = {
+  type: '',
+  price: '',
+  neighborhood: '',
+  zipcode: '',
+  sqft: '',
+  bed: '',
+  bath: '',
+}
+
+const ListingArray = [];
+const App = () => {
+  const [state, setState] = useState(initialState);
+
+  const setValue = (mainKey, text) => {
+    setState(prev => ({ ...prev, [mainKey]: text }));
   }
 
-const RANDOM_COLOR_ARRAY = [
-  {
-    backgroundColor: '#26de81',
-    value: 1,
-  },
-  {
-    backgroundColor: '#fc5c65',
-    value: 2,
-  },
-  {
-    backgroundColor: '#fed330',
-    value: 3,
-  },
-  {
-    backgroundColor: '#4b7bec',
-    value: 4,
-  },
-  {
-    backgroundColor: 'defaultStyles.colors.medium',
-    value: 5,
-  },
-];
-
-const SimplePicker = () => {
-  const [state, setState] = useState({
-    isModalVisble: false,
-    selectedColor: 'gray',
-  });
-
-  const toggleModal = () => {
-    setState(prev => ({...prev, isModalVisble: !prev.isModalVisble}));
-  };
-
-  const setColor = color => {
-    setState(prev => ({
-      ...prev,
-      selectedColor: color,
-      isModalVisble: !prev.isModalVisble,
-    }));
-  };
-
-  const CloseIcon = () => {
-    return (
-      <TouchableOpacity onPress={toggleModal} style={styles.closeIconContainer}>
-        <Text style={styles.crossIcon}>X</Text>
-      </TouchableOpacity>
-    );
-  };
-
-  const renderItem = ({item}) => {
-    if(isColor(item?.backgroundColor)){
-      return null;
-    }
-    return (
-      <TouchableOpacity
-        onPress={() => setColor(`${item?.backgroundColor}`)}
-        style={[styles.pickCircle, {backgroundColor: `${item?.backgroundColor}`}]}>
-        <Text style={styles.pickerText}>{`${item?.backgroundColor}`}</Text>
-      </TouchableOpacity>
-    );
-  };
-
-  const ColorsList = () => {
-    return (
-      <View style={styles.modalBackdrop}>
-        <View style={styles.modalContainer}>
-          <CloseIcon />
-          <FlatList
-            numColumns={2}
-            contentContainerStyle={styles.flatListContent}
-            style={styles.flatListStyle}
-            data={RANDOM_COLOR_ARRAY}
-            renderItem={renderItem}
-            keyExtractor={(_, index) => index.toString()}
-          />
-        </View>
-      </View>
-    );
-  };
+  const submit = () => {
+    ListingArray.push({ id: ListingArray?.length, ...state });
+    setState(initialState);
+  }
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity
-        onPress={toggleModal}
-        style={[styles.circleButton, {backgroundColor: state.selectedColor}]}>
-        <Text style={styles.pickerText}>Picker</Text>
-      </TouchableOpacity>
-      <Modal visible={state.isModalVisble} animationType={'fade'}>
-        <ColorsList />
-      </Modal>
+      <Text style={styles.headText}>Add your listing</Text>
+      <View style={styles.row}>
+        <TextInput placeholderTextColor={'rgba(0, 0, 0, 0.3)'} placeholder='Type' style={styles.textInput} value={state.type} onChangeText={(text) => setValue('type', text)} />
+        <TextInput keyboardType='numeric' placeholderTextColor={'rgba(0, 0, 0, 0.3)'} placeholder='Sq Feet' style={styles.textInput} value={state.sqft} onChangeText={(text) => setValue('sqft', text)} />
+      </View>
+      <View style={styles.row}>
+        <TextInput keyboardType='numeric' placeholderTextColor={'rgba(0, 0, 0, 0.3)'} placeholder='Bedrooms' style={styles.textInput} value={state.bed} onChangeText={(text) => setValue('bed', text)} />
+        <TextInput keyboardType='numeric' placeholderTextColor={'rgba(0, 0, 0, 0.3)'} placeholder='Bathrooms' style={styles.textInput} value={state.bath} onChangeText={(text) => setValue('bath', text)} />
+      </View>
+      <View style={styles.row}>
+        <TextInput keyboardType='numeric' placeholderTextColor={'rgba(0, 0, 0, 0.3)'} placeholder='Price' style={styles.textInput} value={state.price} onChangeText={(text) => setValue('price', text)} />
+        <TextInput keyboardType='numeric' placeholderTextColor={'rgba(0, 0, 0, 0.3)'} placeholder='Zip Code' style={styles.textInput} value={state.zipcode} onChangeText={(text) => setValue('zipcode', text)} />
+      </View>
+      <View style={styles.row}>
+        <TextInput placeholderTextColor={'rgba(0, 0, 0, 0.3)'} placeholder='Neighborhood' style={[styles.textInput, { flex: 1 }]} value={state.neighborhood} onChangeText={(text) => setValue('neighborhood', text)} />
+      </View>
+      <View style={styles.row}>
+        <TouchableOpacity onPress={submit} style={styles.buttonStyle}>
+          <Text style={styles.buttonText}>ADD</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingTop: 20
   },
-  circleButton: {
-    width: 200,
-    height: 200,
-    borderRadius: 100,
-    justifyContent: 'center',
-    alignItems: 'center',
+  headText: {
+    fontSize: 15
   },
-  pickCircle: {
-    width: 90,
-    height: 90,
-    borderRadius: 50,
-    margin: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
   },
-  pickerText: {
+  textInput: {
+    width: '48%',
+    marginTop: 10,
+    backgroundColor: 'rgba(0, 0, 0, 0.05)',
+    borderRadius: 10,
+    paddingLeft: 20
+  },
+  buttonStyle: {
+    flex: 1,
+    borderRadius: 10,
+    backgroundColor: '#12273a',
+    marginTop: 10,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    fontSize: 15,
     color: 'white',
-  },
-  modalBackdrop: {
-    backgroundColor: 'rgba(0, 0, 0, 0.3)',
-    justifyContent: 'center',
-    alignItems: 'center',
-    flex: 1,
-  },
-  modalContainer: {
-    backgroundColor: 'white',
-    width: '90%',
-    height: '50%',
-    borderRadius: 20,
-  },
-  crossIcon: {
-    fontSize: 20,
-  },
-  closeIconContainer: {
-    alignSelf: 'flex-end',
-    padding: 10,
-    paddingRight: 15,
-    position: 'absolute',
-    zIndex: 1,
-  },
-  flatListStyle: {
-    flex: 1,
-  },
-  flatListContent: {
-    flexGrow: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+    fontWeight: '600',
+    padding: 13,
+  }
+})
 
-export default SimplePicker;
+export default App;
